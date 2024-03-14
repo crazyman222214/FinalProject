3deac2bb5a7d94e5f19ca43bcc0e1b591a4370ca
crazyman222214 <schadejr617@gmail.com>
2024-03-13 22:32:28 -0500

    slideshow

--- js/skill.js
+++ js/skill.js
@@ -1,6 +1,11 @@
 let clipSlideshow = document.getElementById("clipSlideshow");
-let clipsFilePath = [""];
+let clipsFilePath = ["./clips/clip1.mp4", "./clips/clip2.mp4", "./clips/clip3.mp4"];
+let currentClip = 0;
 
 clipSlideshow.onended = (event) => {
-    
+    currentClip++;
+    if (currentClip == clipsFilePath.length) {
+        currentClip = 0;
+    }
+    clipSlideshow.src = clipsFilePath[currentClip];
 };
